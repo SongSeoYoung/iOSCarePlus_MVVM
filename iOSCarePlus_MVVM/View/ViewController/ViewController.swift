d33c55e56af6fdfa13f1a5dedcf606fb1a4fd680
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-16 17:05:57 +0900

    [feat] homeView Animation

--- iOSCarePlus_MVVM/View/ViewController/ViewController.swift
+++ iOSCarePlus_MVVM/View/ViewController/ViewController.swift
@@ -8,6 +8,7 @@ import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet private weak var backgroundImage: UIImageView!
     @IBOutlet private weak var appView: UIView!
     @IBOutlet private weak var appTitleLabel: UILabel!
@@ -18,6 +19,15 @@ class ViewController: UIViewController {
         setUI()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        appearLogoViewAnimation { [weak self] in
+            self?.slideBackgroundImageAnimation()
+            self?.blinkLogoAnimation()
+        }
+    }
+    
+    // MARK: - Set UI & Layout
     private func setLayout() {
         backgroundImage.snp.makeConstraints {
             $0.bottom.top.equalToSuperview()
@@ -38,5 +48,42 @@ class ViewController: UIViewController {
     private func setUI() {
         self.appView.layer.cornerRadius = 8.0
     }
+
 }
 
+// MARK: - Animation
+extension ViewController {
+    
+    private func appearLogoViewAnimation(completion: @escaping () -> Void) {
+        UIView.animate(
+            withDuration: 0.7,
+            delay: 1,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 1,
+            options: []) { [weak self] in
+            self?.appView.snp.updateConstraints {
+                $0.top.equalToSuperview().offset(61)
+            }
+            self?.view.layoutIfNeeded()
+        }completion: { _ in
+            completion()
+        }
+    }
+    private func slideBackgroundImageAnimation() {
+        UIView.animate(withDuration: 5,
+                       delay: 0,
+                       options: [.curveEaseInOut, .repeat, .autoreverse]) { [weak self] in
+            self?.backgroundImage.snp.updateConstraints {
+                if let safeAreaLayoutGuide: UILayoutGuide = self?.view.safeAreaLayoutGuide {
+                    $0.leading.equalTo(safeAreaLayoutGuide).offset(-800)
+                }
+            }
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    private func blinkLogoAnimation() {
+        UIView.animate(withDuration: 1, delay: 0, options: [.repeat, .autoreverse]) {
+            [weak self] in self?.appView.alpha = 0 }
+    }
+}
